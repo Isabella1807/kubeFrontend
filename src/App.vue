d8b56ee0b8870f8f326a5ecd9f59a9adad92ea2d
Isabella1807 <megaoreomuffin@hotmail.com>
2024-11-03 22:01:27 +0100

    some css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script setup>
-import {RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
+import TheHeader from "@/components/TheHeader.vue";
 </script>
 
 <template>
-
-  <RouterView />
+  <div class="body_container">
+    <TheHeader/>
+    <div class="main_container">
+      <main>
+        <RouterView/>
+      </main>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.body_container {
+  min-height: 100vh;
+  background-color: deeppink;
+  display: flex;
+  flex-direction: column;
+
+  .main_container {
+    background-color: orange;
+    flex: 1;
+    display: flex;
+    justify-content: space-around;
+
+    main {
+      max-width: 1200px;
+      background-color: green;
+      flex: 1;
+      padding: 16px 24px;
+    }
+  }
+}
 </style>
---
+++ src/views/about.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h2>tester</h2>
+</template>
+
+<style scoped>
+
+</style>
