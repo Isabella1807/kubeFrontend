50cbdb583d897546c15faf2844e2b7f7819d2b47
Carina <54623517+chvessel@users.noreply.github.com>
2024-11-06 21:55:12 +0100

    tilføjet ListTemplates
    tilføjet elementer og styling til at liste templates

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCircleXmark, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faFloppyDisk, faPlus, faTrashCan, faPenSquare,faSortDown } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 
 library.add(faCircleXmark)
 library.add(far)
 library.add(faFloppyDisk)
+library.add(faPlus)
+library.add(faPenSquare)
+library.add(faTrashCan)
+library.add(faSortDown)
 
 const app = createApp(App)
 
