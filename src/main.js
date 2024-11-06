5b623c5dedbb8e89dbc3dd8097746d2b98c8a931
Carina <54623517+chvessel@users.noreply.github.com>
2024-11-06 17:12:05 +0100

    tilføjet styling og elementer
    tilføjet styling og elementer til template og installeret fontawesome ikoner

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import './main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCircleXmark, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faCircleXmark)
+library.add(far)
+library.add(faFloppyDisk)
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
