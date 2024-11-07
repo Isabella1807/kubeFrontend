71ec18514b1fac5d0d0e8d55252ca8825a55f1d0
Isabella1807 <megaoreomuffin@hotmail.com>
2024-11-07 15:02:03 +0100

    fixing minor problems in routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,22 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Login from '../views/Login.vue';
-import ForgotPassword from '../views/ForgotPassword.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            name: 'home',
-            component: HomeView
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/Login.vue')
         },
         {
             path: '/templates',
             name: 'templates',
-            component: Templates
-        },
-        {
-            path: '/',
-            name: 'Login',
-            component: Login
+            component: () => import('../views/Templates.vue')
         },
         {
             path: '/forgot-password',
             name: 'ForgotPassword',
-            component: ForgotPassword
+            component: () => import('../views/ForgotPassword.vue')
         },
       {
           path: '/projects',
