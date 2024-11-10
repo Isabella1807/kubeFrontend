3f072774c03bfe07fa33046aabc9079aef29de33
Ann-Cathrine <58133322+AnnCathrine23@users.noreply.github.com>
2024-11-10 17:53:22 +0100

    UPDATE STYLE

--- src/components/Modal_NewProject.vue
+++ src/components/Modal_NewProject.vue
@@ -77,3 +77,161 @@ export default {
 };
 </script>
 
+<style modal>
+
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+
+.modal-content {
+    background-color: white;
+    padding: 30px;
+    width: 500px;
+    max-width: 90%;
+    border-radius: 10px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    position: relative;
+    text-align: center;
+}
+
+.modal-close {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: none;
+    border: none;
+    font-size: 20px;
+    cursor: pointer;
+    color: #5c007e;
+}
+
+h1 {
+    font-size: 35px;
+    margin-bottom: 25px;
+    font-weight: 700;
+    font-family: 'Poppins';
+    text-align: left;
+}
+
+label {
+    display: block;
+    font-size: 15px;
+    margin-bottom: 10px;
+    text-align: left;
+    font-weight: 400;
+    font-family: 'Poppins';
+}
+
+input, .custom-select {
+    width: 100%;
+    padding: 8px;
+    margin-top: 5px;
+    font-size: 14px;
+    font-style: italic;
+    border: 1px solid #EEEEEE;
+    border-radius: 5px;
+    box-sizing: border-box;
+    color: #333;
+}
+.select-container {
+  position: relative;
+}
+
+.custom-select {
+  appearance: none;
+  padding-right: 30px;
+  color: #333;
+  background-color: white;
+}
+
+.dropdown-icon {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+  color: #5C007E;
+}
+
+.buttons {
+  display: flex;
+  justify-content: flex-start; 
+  gap: 10px; 
+  margin-top: 25px;
+}
+
+.cancel-button, .save-button {
+  padding: 6px 12px;  
+  font-size: 15px;    
+  border-radius: 10px; 
+  width: auto;        
+  font-family: 'Poppins';
+  font-weight: 700;
+}
+
+.cancel-button {
+  background-color: white;
+  color: #5C007E;
+  border: 1px solid #5C007E;
+  cursor: pointer
+}
+
+.save-button {
+  background-color: #5C007E;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+/* Hover effects */
+.cancel-button:hover {
+  background-color: #eee;  
+  color: #333;  
+  border-color: #eee;  
+}
+
+.save-button:hover {
+  background-color: #3700b3;  
+  color: white;  
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);  
+}
+
+
+
+/* Responsive Styles */
+@media (max-width: 768px) {
+  .modal-content {
+    width: 90%;
+    padding: 20px;
+  }
+}
+
+@media (max-width: 480px) {
+  .modal-content {
+    width: 95%;
+    padding: 15px;
+  }
+
+  h1 {
+    font-size: 25 px;
+  }
+
+  .buttons {
+    flex-direction: column;
+  }
+
+  .buttons button {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+}
+</style>
