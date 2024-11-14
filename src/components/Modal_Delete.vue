4c9ac2602d9eb2b838b94d05970199fe2e689407
Ann-Cathrine <58133322+AnnCathrine23@users.noreply.github.com>
2024-11-14 20:45:03 +0100

    update template - modal delete

--- src/components/Modal_Delete.vue
+++ src/components/Modal_Delete.vue
@@ -1,5 +1,15 @@
 <template>
-
+<div class="deletemodal-overlay">
+    <div class="deletemodal">
+     <button class="deleteclose-button">✖</button>
+     <h1 class="deletemodal-title">Are you sure you want to delete <span class="highlight">portfolio?</span></h1>
+     <div class="deletemodal-action">
+        <button class="deletecancel-button">
+          <span>✖</span> No cancel
+        </button>
+     </div>   
+    </div>
+</div>
 </template>
 
 <script></script>
