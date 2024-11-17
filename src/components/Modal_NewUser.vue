5230b7451d2c691c49f54791ecb190fab0e66bc5
Ann-Cathrine <58133322+AnnCathrine23@users.noreply.github.com>
2024-11-17 10:09:30 +0100

    update modal user with template

--- src/components/Modal_NewUser.vue
+++ src/components/Modal_NewUser.vue
@@ -1,3 +1,44 @@
-<template></template>
+<template>
+    <div class="newuser-modal">
+        <div class="newuser-modal-content">
+            <button class="newuser-close-btn" @click="emitClose">✖</button>
+            <h1>New User</h1>
+            <div class="newuser-upload-section">
+                <div class="newuser-upload-button">
+                    <label class="newuser-upload-label">
+                        <i class="fa solid fa-cloud-arrow-up"></i>
+                        <span class="newuser-upload-text">
+                            {{ filename || "Upload .CSV file" }}
+                        </span>
+                        <input type="file" accept=".csv" @change="handleFileOpload" hidden />
+                    </label>
+                    <button v-if="fileName" close="newuser-delete-btn" @click="clearFile" title="Delete File">✖</button>
+                </div>
+            </div>
+            <div class="newuser-checkbox-section">
+                <label class="newuser-checkbox-item">
+                    <input type="checkbox" v-model="isStudent" @change="resetFields" />
+                    Students
+                </label>
+                <label class="newuser-checkbox-item">
+                    <input type="checkbox" v-model="isTeacher" @change="resetFields" />
+                    Teachers
+                </label>
+            </div>
+            <div v-if="isStudent" class="group-input-section">
+                <input type="text" v-model="groupName" placeholder="Group name" class="group-input" />
+            </div>
+            <div class="newuser-button-section">
+                <button class="newuser-cancel-btn" @click="emitClose">
+                    <i class="fa-solid fa-times"></i> Cancel
+                </button>
+                <button class="newuser-save-btn" :disabled="!canSave" @click="saveGroup">
+                    <i class="fa-solid fa-floppy-disk"></i> Save Group
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script></script>
 <style lang="scss"></style>
