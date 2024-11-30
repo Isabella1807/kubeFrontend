ba2d4b9a776b39c81644e491ae2d50ed6d7d673e
Carina <54623517+chvessel@users.noreply.github.com>
2024-11-30 10:46:47 +0100

    Tilføjet API service
    Tilføjet API service, samt konfigureret den til at bruge localhost:3000 som base url.
    Samt tilføjet udtræk med den i templates.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCircleXmark, faFloppyDisk, faPlus, faTrashCan, faPenSquare,faSortDown } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import ApiService from './services/apiService.js';
 
 library.add(faCircleXmark)
 library.add(far)
@@ -16,6 +17,10 @@ library.add(faPlus)
 library.add(faPenSquare)
 library.add(faTrashCan)
 library.add(faSortDown)
+
+
+// Set the API base URL
+ApiService.init('http://localhost:3000');
 
 
 const app = createApp(App)
